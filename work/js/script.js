d3ab381e0fa2ade8f8e2f35b46d641b2563733f7
chikos <68629026+chikos@users.noreply.github.com>
2020-10-03 16:22:17 +0900

    oo

--- work/js/script.js
+++ work/js/script.js
@@ -1,8 +1,16 @@
+$(function(){
+  
+  
+  $(".img_button").on("click",function(){
+    let target = $(this).siblings(".content");
+    target.slideToggle("slow");
 
-$('#wave').wavify({
-    height: 60,
-    bones: 3,
-    amplitude: 40,
-    color: '#0bd',
-    speed: .25
-  });
+    $("html, body").animate({scrollTop:target.offset().top}, "slow");
+  })
+
+  
+  $(".close_button").on("click",function(){
+    $(this).parent(".content").slideUp("slow");
+  })
+
+});
